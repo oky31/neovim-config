1f0dc37afd66aa30192aa3e071e77d8db3007a5d
oky31 <oky.saputra@majoo.id>
2023-12-23 12:55:46 +0700

    add rosepoine

--- after/plugin/rosepine.lua
+++ after/plugin/rosepine.lua
@@ -1,19 +1,17 @@
-require('rose-pine').setup({
-    disable_background = true,
-    disable_italics = true,
-})
+ require('rose-pine').setup({
+     disable_background = true,
+     disable_italics = true,
+ })
+ vim.cmd('colorscheme rose-pine')
+
+ function MyColor(color)
+     vim.cmd.colorscheme(color)
+
+     vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+     vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
+
+ end
+
+ MyColor()
 
 
-vim.cmd('colorscheme rose-pine-moon')
---
--- function MyColor(color)
---     vim.cmd.colorscheme(color)
---
---     vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
---     vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
---
--- end
---
--- MyColor()
---
---
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
